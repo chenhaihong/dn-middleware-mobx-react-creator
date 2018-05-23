dad40d6611820dce40a09dc414899db726551d47
chh <1194445212@qq.com>
2018-05-23 11:36:12 +0800

    1.添加生成模型模板逻辑

---
+++ build/HelloWorld.js
@@ -0,0 +1,19 @@
+import { observable, computed, autorun } from 'mobx';
+
+export default class HelloWorld  {
+  // 监听属性示例
+  // @observable id = 0;
+  // @observable username = '';
+
+  // 计算属性示例
+  // @computed get pure_username() {
+  //   const reg = /s+/ig;
+  //   return this.username.replace(reg, '');
+  // } 
+};
+
+// 衍生：自动运行函数示例
+// autorun(() => {
+//   const name = HelloWorld .username;
+//   console.log(name);
+// });
